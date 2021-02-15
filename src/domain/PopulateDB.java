7a049b100a4c8d1a70f94a93582f45dcf783da21
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-02-15 16:51:28 +0100

    Created class to populate the DB

---
+++ src/domain/PopulateDB.java
@@ -0,0 +1,23 @@
+package domain;
+
+import repository.UserDaoJpa;
+
+public class PopulateDB {
+    public void run() {
+        UserDaoJpa userDao = new UserDaoJpa();
+        UserDaoJpa.startTransaction();
+
+        userDao.insert(new Administrator("thomas123", "Passwd123&", "Thomas", "Dirven"));
+        userDao.insert(new Administrator("sibian123", "Passwd123&", "Sibian", "De Gussem"));
+        userDao.insert(new Administrator("isaac123", "Passwd123&", "Isaac", "Bauters"));
+        userDao.insert(new Administrator("florian123", "Passwd123&", "Florian", "Goossens"));
+        userDao.insert(new Technician("technician01", "Passwd123&", "Joost", "Jansens"));
+        userDao.insert(new SupportManager("supportman01", "Passwd123&", "Joe", "Biden"));
+        userDao.insert(new Customer("customer01", "Passwd123&", "Emma", "Dupont"));
+        userDao.insert(new Customer("customer02", "Passwd123&", "Barak", "Obama"));                                                 
+        userDao.insert(new Employee("employee01", "Passwd123&", "Donald", "Trump", "Straat", "PhoneNumber", "email", "role")); 
+        userDao.insert(new Employee("employee02", "Passwd123&", "John", "Smiths", "Straat", "PhoneNumber", "email", "role"));
+        
+        UserDaoJpa.commitTransaction();
+    }
+}
