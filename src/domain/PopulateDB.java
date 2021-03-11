224f069e18f43439589436fb6e97549d8a693cad
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-03-11 15:25:30 +0100

    Update PopulateDB (extra customers and contracts)

--- src/domain/PopulateDB.java
+++ src/domain/PopulateDB.java
@@ -14,37 +14,90 @@ public class PopulateDB {
         userDao.startTransaction();
         
         ActemiumCompany theWhiteHouse = new ActemiumCompany("The White House", "America 420", "911");
+        ActemiumCompany naessensNV = new ActemiumCompany("Construct Willy Naessens", "Bedrijvenpark Coupure 15, 9700 Oudenaarde", "911");
+        ActemiumCompany google = new ActemiumCompany("Google", "1600 Amphitheatre Parkway, Mountain View, California, United States", "240217996");
+        ActemiumCompany microsoft = new ActemiumCompany("Microsoft", "America 420", "411048476");
+        ActemiumCompany facebook = new ActemiumCompany("Facebook", "America 420", "039234181");
+        ActemiumCompany amazon = new ActemiumCompany("Amazon", "America 420", "475116936");
+        ActemiumCompany tesla = new ActemiumCompany("Tesla", "America 420", "892645557");
+        
+        ActemiumContractType bCtype = new ActemiumContractType("BasicSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.WORKINGHOURS, 5, 2, 999.99);
+        ActemiumContractType bCtype02 = new ActemiumContractType("BasicEmailSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.WORKINGHOURS, 5, 2, 999.99);
+        ActemiumContractType cCtype = new ActemiumContractType("BasisAppSupport", ContractTypeStatus.ACTIVE, false, false, true, Timestamp.WORKINGHOURS, 5, 2, 1299.99);
+        ActemiumContractType dCtype = new ActemiumContractType("FullEmailSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.ALWAYS, 4, 1, 1999.99);
+        ActemiumContractType eCtype = new ActemiumContractType("FullPhoneSupport", ContractTypeStatus.ACTIVE, false, true, false, Timestamp.ALWAYS, 4, 1, 2999.99);
+        ActemiumContractType fCtype = new ActemiumContractType("FullAppSupport", ContractTypeStatus.ACTIVE, false, false, true, Timestamp.ALWAYS, 4, 1, 2299.99);
+        ActemiumContractType gCtype = new ActemiumContractType("BasisAllSupport", ContractTypeStatus.ACTIVE, true, true, true, Timestamp.WORKINGHOURS, 4, 2, 2999.99);
+        ActemiumContractType hCtype = new ActemiumContractType("FullAllSupport", ContractTypeStatus.ACTIVE, true, true, true, Timestamp.ALWAYS, 3, 1, 3999.99);
+        ActemiumContractType jCtype = new ActemiumContractType("AmazonSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.WORKINGHOURS, 5, 2, 999.99);
+        ActemiumContractType jCtype02 = new ActemiumContractType("MonopolySupport", ContractTypeStatus.ACTIVE, true, true, true, Timestamp.ALWAYS, 3, 1, 6969.69);
+        ActemiumContractType pCtype = new ActemiumContractType("BasisPhoneSupport", ContractTypeStatus.ACTIVE, false, true, false, Timestamp.WORKINGHOURS, 5, 2, 1999.99);
+        
         ActemiumCustomer barak = new ActemiumCustomer("cust01Barak", "Passwd123&", "Barak", "Obama", theWhiteHouse);
-        ActemiumContractType bCtype = new ActemiumContractType("BasicEmailSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.WORKINGHOURS, 5, 2, 999.99);
+        ActemiumCustomer jeff = new ActemiumCustomer("cust02Jeff", "Passwd123&", "Jeff", "Bezos", amazon);
+        ActemiumCustomer mark = new ActemiumCustomer("cust03Mark", "Passwd123&", "Mark", "Zuckerberg", facebook);
+        ActemiumCustomer bill = new ActemiumCustomer("cust04Bill", "Passwd123&", "Bill", "Gates", microsoft);
+        ActemiumCustomer larry = new ActemiumCustomer("cust05Larry", "Passwd123&", "Larry", "Page", google);
+        ActemiumCustomer elon = new ActemiumCustomer("cust06Elon", "Passwd123&", "Elon", "Musk", tesla);        
+        
         ActemiumContract bContract = new ActemiumContract(bCtype, barak, LocalDate.now().plusYears(1));
+        ActemiumContract bContract02 = new ActemiumContract(bCtype02, barak, LocalDate.now().plusYears(1));
+        ActemiumContract jContract = new ActemiumContract(jCtype, jeff, LocalDate.now().plusYears(3));
+        ActemiumContract jContract02 = new ActemiumContract(jCtype02, jeff, LocalDate.now().plusYears(2));
+        ActemiumContract mContract = new ActemiumContract(cCtype, mark, LocalDate.now().plusYears(1));
+        ActemiumContract mContract02 = new ActemiumContract(gCtype, mark, LocalDate.now().plusYears(1));
+        ActemiumContract gContract = new ActemiumContract(dCtype, bill, LocalDate.now().plusYears(1));
+        ActemiumContract gContract02 = new ActemiumContract(dCtype, bill, LocalDate.now().plusYears(1));
+        ActemiumContract lContract = new ActemiumContract(eCtype, larry, LocalDate.now().plusYears(3));
+        ActemiumContract lContract02 = new ActemiumContract(hCtype, larry, LocalDate.now().plusYears(3));
+        ActemiumContract eContract = new ActemiumContract(fCtype, elon, LocalDate.now().plusYears(2));
+        ActemiumContract eContract02 = new ActemiumContract(pCtype, elon, LocalDate.now().plusYears(2));        
+        
         barak.addContract(bContract);
-        ActemiumCustomer barak2 = new ActemiumCustomer("cust02Barak", "Passwd123&", "Barak", "Obama", theWhiteHouse);
-        ActemiumCustomer barak3 = new ActemiumCustomer("cust03Barak", "Passwd123&", "Barak", "Obama", theWhiteHouse);
-        ActemiumCustomer barak4 = new ActemiumCustomer("cust04Barak", "Passwd123&", "Barak", "Obama", theWhiteHouse);
-        ActemiumCustomer barak5 = new ActemiumCustomer("cust05Barak", "Passwd123&", "Barak", "Obama", theWhiteHouse);
-        barak4.setStatus(UserStatus.BLOCKED);
-        barak5.setStatus(UserStatus.INACTIVE);
-        ActemiumEmployee tech = new ActemiumEmployee("Technician", "Passwd123&", "Johan", "Van Schoor","Overwale 42","091354864","thomas.dirven@hogent.be", EmployeeRole.TECHNICIAN);
+        barak.addContract(bContract02);
+        
+        jeff.addContract(jContract);
+        jeff.addContract(jContract02);
+        
+        mark.addContract(mContract);
+        mark.addContract(mContract02);
+        
+        bill.addContract(gContract);
+        bill.addContract(gContract02);
+        
+        larry.addContract(lContract);
+        larry.addContract(lContract02);
+        
+        elon.addContract(eContract);
+        elon.addContract(eContract02);
+        
+        mark.setStatus(UserStatus.BLOCKED);
+        larry.setStatus(UserStatus.INACTIVE);
+        ActemiumEmployee tech = new ActemiumEmployee("Technician", "Passwd123&", "Joe", "Biden","Overwale 42","091354864","thomas.dirven@hogent.be", EmployeeRole.TECHNICIAN);
         ActemiumEmployee tech2 = new ActemiumEmployee("Technician", "Passwd123&", "Donald", "Trump","Overwale 42","091354864","thomas.dirven@hogent.be", EmployeeRole.TECHNICIAN);
 
         userDao.insert(tech);
         userDao.insert(tech2);
         ActemiumTicket ticket01 = new ActemiumTicket(TicketPriority.P1,TicketType.DATABASE, "Ticket1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", barak);
-        ActemiumTicket ticket02 = new ActemiumTicket(TicketPriority.P1, TicketType.HARDWARE,"Ticket2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", barak);
-        ActemiumTicket ticket03 = new ActemiumTicket(TicketPriority.P1, TicketType.INFRASTRUCTURE, "Ticket3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", barak);
-        ActemiumTicket ticket04 = new ActemiumTicket(TicketPriority.P1, TicketType.NETWORK, "Ticket4", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", barak);
-        ActemiumTicket ticket05 = new ActemiumTicket(TicketPriority.P1, TicketType.SOFTWARE, "Ticket5", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", barak);
+        ActemiumTicket ticket02 = new ActemiumTicket(TicketPriority.P1, TicketType.HARDWARE,"Ticket2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", jeff);
+        ActemiumTicket ticket03 = new ActemiumTicket(TicketPriority.P1, TicketType.INFRASTRUCTURE, "BBQ grill stopt working", "I was smoking these meats with sweet baby rays saus and suddenly my BBQ stopped working", mark);
+        ActemiumTicket ticket04 = new ActemiumTicket(TicketPriority.P1, TicketType.NETWORK, "Microchips in vaccine not working", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", bill);
+        ActemiumTicket ticket05 = new ActemiumTicket(TicketPriority.P1, TicketType.SOFTWARE, "Ticket5", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", larry);
+        ActemiumTicket ticket06 = new ActemiumTicket(TicketPriority.P1, TicketType.SOFTWARE, "Ticket6", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", elon);
         ticket05.setStatus(TicketStatus.COMPLETED);
         barak.addTicket(ticket01);
-        barak2.addTicket(ticket02);
-        barak3.addTicket(ticket03);
-        barak4.addTicket(ticket04);
-        barak5.addTicket(ticket05);
+        jeff.addTicket(ticket02);
+        mark.addTicket(ticket03);
+        bill.addTicket(ticket04);
+        larry.addTicket(ticket05);
+        elon.addTicket(ticket06);
         ticket01.addTechnician(tech);
         ticket02.addTechnician(tech);
         ticket03.addTechnician(tech);
         ticket04.addTechnician(tech2);
         ticket05.addTechnician(tech2);
+        ticket06.addTechnician(tech);
+        ticket06.addTechnician(tech2);
         userDao.insert(new ActemiumEmployee("Admin123", "Passwd123&", "Johan", "Van Schoor","Overwale 42","091354864","thomas.dirven@hogent.be", EmployeeRole.ADMINISTRATOR));
         userDao.insert(new ActemiumEmployee("Sup123", "Passwd123&", "Johan", "Van Schoor","Overwale 42","091354864","thomas.dirven@hogent.be", EmployeeRole.SUPPORT_MANAGER));
         userDao.insert(new ActemiumEmployee("Tech123", "Passwd123&", "Johan", "Van Schoor","Overwale 42","091354864","thomas.dirven@hogent.be", EmployeeRole.TECHNICIAN));
@@ -59,12 +112,12 @@ public class PopulateDB {
         userDao.insert(badguy);
         userDao.insert(inactive);
         userDao.insert(barak);
-        userDao.insert(barak2);
-        userDao.insert(barak3);
-        userDao.insert(barak4);
-        userDao.insert(barak5);
-        userDao.insert(new ActemiumCustomer("cust02Johan", "Passwd123&", "Johan", "Vercauteren", theWhiteHouse));
-        userDao.insert(new ActemiumCustomer("cust03Emma", "Passwd123&", "Emma", "Dupont", theWhiteHouse));
+        userDao.insert(jeff);
+        userDao.insert(mark);
+        userDao.insert(bill);
+        userDao.insert(larry);
+        userDao.insert(elon);
+        userDao.insert(new ActemiumCustomer("cust02Johan", "Passwd123&", "Willy", "Naessens", naessensNV));
         userDao.insert(new ActemiumEmployee("tech01Donald", "Passwd123&", "Donald", "Trump", "Stationstraat 56", "092548736", "donald.trump@hogent.be", EmployeeRole.TECHNICIAN));
         userDao.insert(new ActemiumEmployee("supman01John", "Passwd123&", "John", "Smiths", "Stationstraat 34", "093504816", "john.smiths@hogent.be", EmployeeRole.SUPPORT_MANAGER));
 
@@ -85,14 +138,6 @@ public class PopulateDB {
         userDao.commitTransaction();
 
         contractTypeDao.startTransaction();
-        contractTypeDao.insert(new ActemiumContractType("BasisEmailSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.WORKINGHOURS, 5, 2, 999.99));
-        contractTypeDao.insert(new ActemiumContractType("BasisPhoneSupport", ContractTypeStatus.ACTIVE, false, true, false, Timestamp.WORKINGHOURS, 5, 2, 1999.99));
-        contractTypeDao.insert(new ActemiumContractType("BasisAppSupport", ContractTypeStatus.ACTIVE, false, false, true, Timestamp.WORKINGHOURS, 5, 2, 1299.99));
-        contractTypeDao.insert(new ActemiumContractType("FullEmailSupport", ContractTypeStatus.ACTIVE, true, false, false, Timestamp.ALWAYS, 4, 1, 1999.99));
-        contractTypeDao.insert(new ActemiumContractType("FullPhoneSupport", ContractTypeStatus.ACTIVE, false, true, false, Timestamp.ALWAYS, 4, 1, 2999.99));
-        contractTypeDao.insert(new ActemiumContractType("FullAppSupport", ContractTypeStatus.ACTIVE, false, false, true, Timestamp.ALWAYS, 4, 1, 2299.99));
-        contractTypeDao.insert(new ActemiumContractType("BasisAllSupport", ContractTypeStatus.ACTIVE, true, true, true, Timestamp.WORKINGHOURS, 4, 2, 2999.99));
-        contractTypeDao.insert(new ActemiumContractType("FullAllSupport", ContractTypeStatus.ACTIVE, true, true, true, Timestamp.ALWAYS, 3, 1, 3999.99));
         contractTypeDao.insert(new ActemiumContractType("ExperimentalSupport", ContractTypeStatus.INACTIVE, true, false, true, Timestamp.ALWAYS, 4, 1, 2999.99));
 		contractTypeDao.commitTransaction();
 
