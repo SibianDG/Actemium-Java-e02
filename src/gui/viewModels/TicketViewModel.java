fa07bfd1fb06a9219ad228393b042958caae1000
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-03-24 22:50:04 +0100

    BugFix Ticket resolved permissions for technician

--- src/gui/viewModels/TicketViewModel.java
+++ src/gui/viewModels/TicketViewModel.java
@@ -71,7 +71,7 @@ public class TicketViewModel extends ViewModel {
         Map<String, Map<Boolean, Object>> details = new LinkedHashMap<>();
         boolean techPermissions = userFacade.getEmployeeRole().equals(EmployeeRole.TECHNICIAN) && TicketStatus.isOutstanding();
         boolean editable = TicketStatus.isOutstanding();
-        if(userFacade.getEmployeeRole().equals(EmployeeRole.TECHNICIAN)) {
+        if(TicketStatus.isOutstanding() && userFacade.getEmployeeRole().equals(EmployeeRole.TECHNICIAN)) {
         	editable = !editable;
         }
         details.put(LanguageResource.getString("title"), Collections.singletonMap(editable, ticket.getTitle()));
